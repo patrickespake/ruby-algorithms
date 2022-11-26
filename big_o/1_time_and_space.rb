19660888421b7076bc95ab595ab668696c565edf
patrick <patrick@envixo.com>
2022-11-26 12:12:50 +0000

    initial study for space complexity Big O

---
+++ big_o/1_time_and_space.rb
@@ -0,0 +1,13 @@
+# Stack space in recursive calls counts as O(n) space.
+# The stack space is the number of recursive calls that are on the stack at any given time.
+# For example, code like this would take O(n) time and O(n) space.
+def sum(n)
+  return 0 if n <= 0
+
+  n + sum(n-1)
+end
+
+# Type the n value on console
+puts "Type the n value: "
+n = gets.chomp.to_i
+sum(n)
