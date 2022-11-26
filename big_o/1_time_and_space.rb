4bf1849d3b533f52ff3edcc2b3ef4179e0d37d68
patrick <patrick@envixo.com>
2022-11-26 12:24:01 +0000

    Stack space in recursive. For example, code like this would take O(n) time and O(n) space.

--- big_o/1_time_and_space.rb
+++ big_o/1_time_and_space.rb
@@ -2,6 +2,8 @@
 # The stack space is the number of recursive calls that are on the stack at any given time.
 # For example, code like this would take O(n) time and O(n) space.
 def sum(n)
+  puts "sum(#{n})"
+
   return 0 if n <= 0
 
   n + sum(n-1)
@@ -10,4 +12,4 @@ end
 # Type the n value on console
 puts "Type the n value: "
 n = gets.chomp.to_i
-sum(n)
+puts "Result: #{sum(n)}"
